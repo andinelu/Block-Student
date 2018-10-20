00e1194052f562f4433f8048b8ea6bdae6f708f7
olejsc <olejsc@stud.ntnu.no>
2018-10-20 13:00:31 +0200

    Added assets for blocks, added playercontrol script

---
+++ Gamejam Student Block/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+    [SerializeField] private float speed;
+    [SerializeField] private float maxHorizontalSpeed;
+    [SerializeField] private float velocityIncrements;
+
+    private bool hasJumped;
+    private bool isGrounded;
+
+    [SerializeField] private LayerMask groundMask;
+
+
+    void Awake()
+    {
+        this.rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
